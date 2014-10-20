2590c93b412087f66b9277a78fa38de9ce297dea
Ted Kaemming <ted@kaemming.com>
2014-10-20 13:28:21 -0700

    0.0.6, adds !important support, Python 3 compatibility improvements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = [
 
 setup(
     name='toronado',
-    version='0.0.5',
+    version='0.0.6',
     author='ted kaemming, disqus',
     author_email='ted@disqus.com',
     packages=find_packages(exclude=('tests',)),
