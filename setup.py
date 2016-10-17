71936faf39de059a86dbdbcd6b0c2379a68eb5d2
Ted Kaemming <ted@kaemming.com>
2016-10-17 14:45:40 -0700

    0.0.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='toronado',
-    version='0.0.10',
+    version='0.0.11',
     description='Fast lxml-based CSS stylesheet inliner.',
     author='ted kaemming, disqus',
     author_email='ted@disqus.com',
